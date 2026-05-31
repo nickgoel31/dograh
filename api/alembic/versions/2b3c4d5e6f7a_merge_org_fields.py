69b85c2d81d3909ab782c287d411b86969da81b2
nickgoel31 <harshgoel2004@gmail.com>
2026-05-31 19:13:13 +0530

    fixed errors for head

--- api/alembic/versions/2b3c4d5e6f7a_merge_org_fields.py
+++ api/alembic/versions/2b3c4d5e6f7a_merge_org_fields.py
@@ -1,26 +1,26 @@
-"""add name slug is_active to organizations
+"""merge org fields
 
-Revision ID: 742a7dcd57ce
-Revises: f89239b21f04
-Create Date: 2026-05-31 13:21:54.000000
+Revision ID: 2b3c4d5e6f7a
+Revises: 1a2b3c4d5e6f, 742a7dcd57ce
+Create Date: 2026-05-31 19:15:00.000000
 
 """
-
 from typing import Sequence, Union
 
-import sqlalchemy as sa
 from alembic import op
+import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = "742a7dcd57ce"
-down_revision: Union[str, None] = "f89239b21f04"
+revision: str = '2b3c4d5e6f7a'
+down_revision: Union[str, Sequence[str], None] = ('1a2b3c4d5e6f', '742a7dcd57ce')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     pass
+
 
 def downgrade() -> None:
     pass
